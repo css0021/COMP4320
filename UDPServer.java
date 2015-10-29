e22c0c87595c2ef4120e596033c2205806332792
Christopher Sheldon Strong <css0021@auburn.edu>
2015-10-29 15:51:54 -0500

    working server

--- UDPServer.java
+++ UDPServer.java
@@ -47,10 +47,9 @@ public class UDPServer {
          	
             short sum = getChecksum(data);
             
-            
             byte errorCode = 0;
-            if(magicNum == 0 || tml <= 7 || sum != 0xFF) {
-               if(magicNum == 0) {
+            if(magicNum != 4660 || tml <= 7 || sum != 0xFF) {
+               if(magicNum != 4660) {
                   errorCode = getByteErrorCode(2);
                   System.out.println("\nMessage is invalid: No magic number.");
                
@@ -65,17 +64,18 @@ public class UDPServer {
                   System.out.println("\nMessage is invalid: Bad checksum.");
                
                }
-               byte[] sendData = new byte[255];
+               
+               byte[] sendData = new byte[ERROR_PACKET_LENGTH];
                sendData[0] = 0x12;
                sendData[1] = 0x34;
-               sendData[2] = (byte) (ERROR_PACKET_LENGTH & 0x00FF);
-               sendData[3] = (byte) ((ERROR_PACKET_LENGTH >> 8) & 0x00FF);
+               sendData[2] = (byte) ((ERROR_PACKET_LENGTH >> 8) & 0x00FF);
+               sendData[3] = (byte) ((ERROR_PACKET_LENGTH >> 0) & 0x00FF);
                sendData[4] = 0;
                sendData[5] = 11;
                sendData[6] = errorCode;
                sendData[4] = (byte) ~(getChecksum(sendData));
                DatagramPacket sendPacket = 
-                  new DatagramPacket(sendData, sendData.length, incoming.getAddress(), port);
+                  new DatagramPacket(sendData, sendData.length, incoming.getAddress(), incoming.getPort());
                servSock.send(sendPacket);
                incoming = null;
                System.out.println("Sent error packet to " + host + " successfully.");
@@ -84,21 +84,22 @@ public class UDPServer {
                
                int totalHosts = 0;
                int i = 7;
-               int newTML = 7 + totalHosts * 4;
-               byte[] send = new byte[255];
-               
-               send[0] = 0x12;
-               send[1] = 0x34;
-               send[2] = (byte) (newTML & 0x00FF);
-               send[3] = (byte) ((newTML >> 8) & 0x00FF);
-               send[4] = 0;
-               send[5] = 11;
-               send[6] = reqID;
                while (i < tml - 1) {
                   int bytesToJump = data[i] + 1;
                   totalHosts += 1;
                   i += bytesToJump;
                }
+               int newTML = 7 + totalHosts * 4;
+               byte[] send = new byte[newTML];
+               
+               send[0] = 0x12;
+               send[1] = 0x34;
+               send[2] = (byte) ((newTML >> 8)& 0x00FF);
+               send[3] = (byte) ((newTML >> 0) & 0x00FF);
+               send[4] = 0;
+               send[5] = 11;
+               send[6] = reqID;
+               
                
                int length = 0;
                int msgLength = 0;
@@ -130,7 +131,7 @@ public class UDPServer {
                }
                send[4] = (byte) ~(getChecksum(send));
                DatagramPacket sendPacket = 
-                  new DatagramPacket(send, send.length, incoming.getAddress(), port);
+                  new DatagramPacket(send, send.length, incoming.getAddress(), incoming.getPort());
                servSock.send(sendPacket);
                incoming = null;
                System.out.println("Sent packet to " + host + " successfully.");
